339ac72c1bf623ae85a9a61f54778eec13b061a5
Tejas <139220864+DRAKKKkk@users.noreply.github.com>
2025-08-14 19:36:46 +0530

    fix app.js code

--- server/src/app.js
+++ server/src/app.js
@@ -13,6 +13,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(morgan("dev")); //HTTP request logger middleware for node.js 
+
 
 export {app};
--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
-import dotenv from "dotenv";
-import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+import {app }from "./app.js"
 
 dotenv.config({
     path: "./env"
@@ -9,9 +10,9 @@ connectDB()
 
 .then(() => {
     app.listen(process.env.PORT || 8000,() => {
-        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        console.log(`Server is running on port ${process.env.PORT || 8000}`)
     })
 })
 .catch((error) => {
-    console.error("MONGODB connection error :", error);
+    console.error("MONGODB connection error :", error)
 });
